8e650d196f0a229c784aed16ac90d5929e7973f7
Charles <cwolfe7@asu.edu>
2021-08-01 17:14:20 -0700

    can detect straights now

--- Assets/Scripts/WinnerFinder.cs
+++ Assets/Scripts/WinnerFinder.cs
@@ -17,36 +17,42 @@ high card = 1
 
 public class WinnerFinder : MonoBehaviour
 {
-    // straight flush combos
-    int[,] straightFlush = new int[,] { { 0, 4, 8, 12, 16 }, { 4, 8, 12, 116, 20 }, { 8, 12, 16, 20, 24 }, {12,16,20,24,28 },
-                                        {16,20,24,28,32 }
-    };
-
    
+    // all possible straight combinations 
+    private char[,] straightCombos = new char[,] { {'a','2','3','4','5'}, {'2','3','4','5','6'}, {'3','4','5','6','7'},
+                                          {'4','5','6','7','8' },{'5','6','7','8','9'}, {'6','7','8','9','t'},
+                                          {'7','8','9','t','j' },{'8','9','t','j','q'}, {'9','t','j','q','k'},
+                                          {'t','j','q','k','a' } };
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     public int CheckHand(GameObject[] hand)
     {
         int pairScore = CheckForPairs(hand);
         bool flush = CheckForFlush(hand);
+        bool straight = CheckAllStraights(hand);
 
         // full house/quads
-        if (pairScore > 6){ return pairScore; }
-        
+        if (pairScore > 6) { return pairScore; }
+
         // flush
         else if (flush) { return 6; }
-        
+
+        // straight
+        else if (straight) { return 5; }
+
         // trips, pairs, high card
         else { return pairScore; }
     }
@@ -59,7 +65,7 @@ public class WinnerFinder : MonoBehaviour
         bool fullHouse = false;
         bool quads = false;
 
-        foreach(GameObject card in hand)
+        foreach (GameObject card in hand)
         {
             // count matches in letter at idx 0 (which is rank)
             int dups = CountDups(card.name[0], 0, hand);
@@ -74,7 +80,7 @@ public class WinnerFinder : MonoBehaviour
         numPairs /= 2;
 
         // check for full house
-        if (numPairs >= 1 && trips){ fullHouse = true; }
+        if (numPairs >= 1 && trips) { fullHouse = true; }
 
         if (quads) { return 8; }
         else if (fullHouse) { return 7; }
@@ -87,17 +93,75 @@ public class WinnerFinder : MonoBehaviour
 
     private bool CheckForFlush(GameObject[] hand)
     {
-        foreach(GameObject card in hand)
+        foreach (GameObject card in hand)
         {
             // count matches in letter at idx 1 (which is suit)
             int suitDups = CountDups(card.name[1], 1, hand);
-            
+
             // count 5 of the same suit, exit function, you have flush
             if (suitDups >= 4) { return true; }
         }
 
         // no flush if never counted 5 of same suit
         return false;
+    }
+
+
+    
+    private bool CheckAllStraights(GameObject[] hand)
+    {
+        // loop through all 10 straight combos
+        for (int i=0; i < 10; i++)
+        {
+            bool straightFound = CheckStraight(straightCombos[i, 0], straightCombos[i, 1], straightCombos[i, 2],
+                                               straightCombos[i, 3], straightCombos[i, 4], hand);
+            
+            // exit the function and return true if you find a straight
+            if (straightFound) { return true; }
+        }
+
+        // if you get to this point, there is no straight
+        return false;
+    }
+
+        
+    
+    
+
+    // helper function to check for a particular straight combo
+    private bool CheckStraight(char r1, char r2, char r3, char r4, char r5, GameObject[] hand)
+    {
+        // take input chars and put the into array
+        char[] straightArr = new char[] { r1, r2, r3, r4, r5 };
+
+        // take first letters (ranks) from player hand and put into array
+        char[] handRanksArr = new char[] {hand[0].name[0], hand[1].name[0], hand[2].name[0], hand[3].name[0],
+                                          hand[4].name[0], hand[5].name[0], hand[6].name[0]};
+
+        // check if player hand contains straight combo
+        if (IsSubset(handRanksArr, straightArr, 5, 7)) { return true; }
+        else { return false; }
+    }
+
+    // helper function to check if a 7 card array contains a certain 5 card array
+    private bool IsSubset(char[] arr1, char[] arr2, int arr2Size, int arr1Size)
+    {
+        int i = 0;
+        int j = 0;
+
+        for (i=0; i<arr2Size; i++)
+        {
+            for (j=0; j<arr1Size; j++)
+            {
+                if (arr2[i] == arr1[j]) { break; }
+            }
+
+            // if the above loop goes once through without breaking, then its false
+            if (j == arr1Size) { return false; }
+        }
+
+        // if we make it here, then arr2 is subset of arr1
+        return true;
     }
 
     private void HandleOverTwoPairs()
